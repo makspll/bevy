0b377994870697979811a0a8b8b9220579af57ad
makspll <makspl17@gmail.com>
2022-07-07 02:52:38 +0100

    from_reflect for Uuid

--- crates/bevy_reflect/src/impls/utils.rs
+++ crates/bevy_reflect/src/impls/utils.rs
@@ -1,7 +1,8 @@
 use crate as bevy_reflect;
 use crate::prelude::ReflectDefault;
 use crate::{ReflectDeserialize, ReflectSerialize};
-use bevy_reflect_derive::{impl_reflect_value};
+use bevy_reflect_derive::{impl_reflect_value, impl_from_reflect_value};
 use bevy_utils::Uuid;
 
 impl_reflect_value!(Uuid(Debug,PartialEq,Serialize,Deserialize,Default));
+impl_from_reflect_value!(Uuid);
